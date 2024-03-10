744211167dab87b7f72ccbaa2480bcad18834324
CRBroughton <crbroughton@posteo.uk>
2024-03-10 07:11:28 +0000

    feat: :heavy_plus_sign: add ESLint and  antfu config to dependencies

--- playground/src/components/HelloWorld.vue
+++ playground/src/components/HelloWorld.vue
@@ -6,81 +6,80 @@ import sql from 'sql.js'
 import { faker } from '@faker-js/faker'
 
 interface tableRowType {
-    id: number
-    name: string
-    sex: string
-    job: string
+  id: number
+  name: string
+  sex: string
+  job: string
 }
 
 const SQL = await sql({
-        locateFile: () => {
-            return '/sql-wasm.wasm'
-        }
-    })
+  locateFile: () => {
+    return '/sql-wasm.wasm'
+  }
+})
 const db = new SQL.Database()
 
-const {  createTable, Insert, Select, All } = await Sibyl<tableRowType>(db,'test')
+const { createTable, Insert, Select, All } = await Sibyl<tableRowType>(db, 'test')
 
 const myResults = ref<QueryExecResult>()
 const selection = ref<tableRowType[]>()
 
-  createTable('id int, name char, sex char, job char')
+createTable('id int, name char, sex char, job char')
 
-  let insertions: tableRowType[] = []
-    for (let index = 0; index < 1000; index++) {
-      insertions.push({
-      id: faker.number.int(),
-      name: `${faker.person.firstName()}`,
-      sex: `${faker.person.sex()}`,
-      job: `${faker.person.jobTitle()}`,
-    })
-  }
-  const test = Insert('test', insertions)
-  db.run(test)
-  
-  const results = db.exec('select * from test')
-  const result = results[0]
-  myResults.value = result
+let insertions: tableRowType[] = []
+for (let index = 0; index < 1000; index++) {
+  insertions.push({
+    id: faker.number.int(),
+    name: `${faker.person.firstName()}`,
+    sex: `${faker.person.sex()}`,
+    job: `${faker.person.jobTitle()}`,
+  })
+}
+const test = Insert('test', insertions)
+db.run(test)
 
-  
+const results = db.exec('select * from test')
+const result = results[0]
+myResults.value = result;
+
+
 const resultsTest = All()
 selection.value = Select({
-   where: {
+  where: {
     sex: 'male',
-   },
-   limit: 20,
+  },
+  limit: 20,
 })
 </script>
 
 <template>
-    <div>
-      {{ selection }}
+  <div>
+    {{ selection }}
   </div>
-<div class="table">
-  <div class="table-row">
-    <div 
-      class="table-cell font-bold pr-2"
-      v-for="column, columnIndex in resultsTest?.columns"
-      :key="columnIndex">
-      {{ column }}
+  <div class="table">
+    <div class="table-row">
+      <div class="table-cell font-bold pr-2" v-for="column, columnIndex in resultsTest?.columns" :key="columnIndex">
+        {{ column }}
+      </div>
+    </div>
+
+    <div class="table-row" v-for="row, rowIndex in myResults?.values" :key="rowIndex">
+      <div class="table-cell" v-for="_value, columnIndex in myResults?.columns" :key="columnIndex">
+        {{ row[columnIndex] }}
+      </div>
     </div>
   </div>
-
-  <div class="table-row" v-for="row, rowIndex in myResults?.values" :key="rowIndex">
-   <div class="table-cell" v-for="_value, columnIndex in myResults?.columns" :key="columnIndex">
-    {{ row[columnIndex] }}
-  </div>
-  </div>
-</div>
 </template>
 
 <style scoped>
 .table {
   display: table;
 }
+
 .table-row {
   display: table-row;
 }
+
 .table-cell {
   display: table-cell;
 }
--- src/tests/insert.test.ts
+++ src/tests/insert.test.ts
@@ -1,55 +1,55 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import sql from 'sql.js'
 import { Sibyl } from '../index'
 
 interface TableRow {
-    id: number
-    name: string
-    location: string
+  id: number
+  name: string
+  location: string
 }
 
 describe('insert tests', () => {
-    test('Correctly formats a single INSERT statement for the DB', async() => {
-        const SQL = await sql({
-            locateFile: () => {
-                return 'playground/public/sql-wasm.wasm'
-            }
-        })
-        const db = new SQL.Database()
-        const { Insert } = await Sibyl<TableRow>(db, 'testing-DB')
-    
-        const actual = Insert('test', [
-            {
-                id: 1,
-                location: "Brighton",
-                name: "Craig"
-            }
-        ])
-    
-        expect(actual).toStrictEqual('INSERT INTO test VALUES (1,"Brighton","Craig");')
+  it('correctly formats a single INSERT statement for the DB', async () => {
+    const SQL = await sql({
+      locateFile: () => {
+        return 'playground/public/sql-wasm.wasm'
+      },
     })
-    test('Correctly formats several INSERT statments for the DB', async() => {
-        const SQL = await sql({
-            locateFile: () => {
-                return 'playground/public/sql-wasm.wasm'
-            }
-        })
-        const db = new SQL.Database()
-        const { Insert } = await Sibyl<TableRow>(db, 'testing-DB')
-    
-        const actual = Insert('test', [
-            {
-                id: 1,
-                location: "Brighton",
-                name: "Craig"
-            },
-            {
-                id: 2,
-                location: "Cornwall",
-                name: "Bob"
-            }
-        ])
+    const db = new SQL.Database()
+    const { Insert } = await Sibyl<TableRow>(db, 'testing-DB')
 
-        expect(actual).toStrictEqual('INSERT INTO test VALUES (1,"Brighton","Craig"); INSERT INTO test VALUES (2,"Cornwall","Bob");')
+    const actual = Insert('test', [
+      {
+        id: 1,
+        location: 'Brighton',
+        name: 'Craig',
+      },
+    ])
+
+    expect(actual).toStrictEqual('INSERT INTO test VALUES (1,"Brighton","Craig");')
+  })
+  it('correctly formats several INSERT statments for the DB', async () => {
+    const SQL = await sql({
+      locateFile: () => {
+        return 'playground/public/sql-wasm.wasm'
+      },
     })
+    const db = new SQL.Database()
+    const { Insert } = await Sibyl<TableRow>(db, 'testing-DB')
+
+    const actual = Insert('test', [
+      {
+        id: 1,
+        location: 'Brighton',
+        name: 'Craig',
+      },
+      {
+        id: 2,
+        location: 'Cornwall',
+        name: 'Bob',
+      },
+    ])
+
+    expect(actual).toStrictEqual('INSERT INTO test VALUES (1,"Brighton","Craig"); INSERT INTO test VALUES (2,"Cornwall","Bob");')
+  })
 })
