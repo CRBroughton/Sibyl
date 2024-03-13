543689dbb7c16b03700fc4fb64b1468957a69abb
CRBroughton <crbroughton@posteo.uk>
2024-03-13 19:57:16 +0000

    feat: :sparkles: improve playground - now features a scrollable table, with a button to create new entries dynamically

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { useDark, useToggle } from '@vueuse/core'
+import DemoTable from './components/DemoTable.vue'
+import { Button } from '@/components/ui/button'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-<Suspense>
-  <HelloWorld/>
-</Suspense>
+  <div class="flex min-h-screen flex-col">
+    <header class="sticky z-40 top-0 backdrop-blur-lg border-b border-border">
+      <div class="container flex h-14 max-w-screen-2xl items-center">
+        <div class="flex gap-5 items-center">
+          <p class="font-bold">
+            Sibyl Playground
+          </p>
+          <Button variant="link" class="font-bold">
+            <a href="https://github.com/crbroughton/sibyl">Github</a>
+          </Button>
+        </div>
+        <div class="ml-auto flex gap-5">
+          <Button @click="toggleDark()">
+            Toggle {{ isDark ? 'Dark' : 'Light' }} Mode
+          </Button>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Suspense>
+        <DemoTable />
+      </Suspense>
+    </div>
+  </div>
 </template>
---
+++ playground/src/components/DemoTable.vue
@@ -0,0 +1,160 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Button } from '@/components/ui/button'
+
+import { All, Order, Select as SelectOrders, Create } from '@/sibyl'
+import { computed, onMounted, ref } from 'vue'
+import { faker } from '@faker-js/faker'
+
+const results = ref<Order[] | undefined>([])
+
+const currencies = ref<string[]>([])
+const selectedCurrency = ref('')
+onMounted(() => {
+  results.value = All('orders')
+  function getCurrencies(orders: Order[]): string[] {
+  currencies.value.push('All')
+
+  orders.forEach(order => {
+    if (!currencies.value.includes(order.currency)) {
+      currencies.value.push(order.currency)
+    }
+  })
+
+  return currencies.value
+}
+if (results.value !== undefined) {
+  getCurrencies(results.value)
+}
+})
+
+function filterOrders() {
+  if (selectedCurrency.value !== 'All') {
+    results.value = SelectOrders('orders', {
+    where: {
+      currency: selectedCurrency.value
+    }
+  })
+  } else {
+    results.value = All('orders')
+  }
+}
+
+const tableClasses = computed(() => {
+  if (results.value!.length <= 15)
+    return ''
+
+  if (results.value!.length === 1)
+    return ''
+
+  if (results.value!.length > 0)
+    return 'block overflow-scroll h-96 min-h-96'
+  return ''
+})
+
+function createNewEntry() {
+  Create('orders', {
+    id: faker.number.int(),
+    product: faker.commerce.product(),
+    currency: faker.finance.currency().name,
+    price: faker.commerce.price(),
+  })
+  results.value = All('orders')
+}
+
+const amountOfOrders = computed(() => {
+  return results.value && results.value.length
+})
+</script>
+
+<template>
+  <div v-if="results" class="my-6 lg:w-[980px]">
+    <Card class="w-full lg:w-[980px]">
+      <CardHeader>
+        <CardTitle>
+            Sybil Table
+        </CardTitle>
+        <CardDescription>
+          Order amount: {{ amountOfOrders }}
+        </CardDescription>
+        <div class="my-2">
+          <Select v-model="selectedCurrency">
+            <SelectTrigger>
+                <SelectValue placeholder="Currency Filter" />
+            </SelectTrigger>
+            <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Currencies</SelectLabel>
+                <div v-for="c in currencies">
+                  <SelectItem :value="c" @click="filterOrders">
+                    {{ c }}
+                  </SelectItem>
+                </div>
+            </SelectGroup>
+            </SelectContent>
+        </Select>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <Table class="mb-6" :class="tableClasses">
+          <TableHeader>
+            <TableRow>
+              <TableHead class="w-[25%]">
+                ID
+              </TableHead>
+              <TableHead class="w-[25%]">
+                Currency
+              </TableHead>
+              <TableHead class="w-[25%]">
+                Product
+              </TableHead>
+              <TableHead class="w-[25%]">
+                Price
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="row in results" :key="row.id">
+              <TableCell class="font-medium">
+                {{ row.id }}
+              </TableCell>
+              <TableCell>
+                {{ row.currency }}
+              </TableCell>
+              <TableCell>{{ row.product }}</TableCell>
+              <TableCell>{{ row.price }}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <div>
+          <Button @click="createNewEntry">
+            Create New Entry
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</template>
