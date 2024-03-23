5ce7fcc5955194e2edd28f92da1c89e0516896ec
CRBroughton <crbroughton@posteo.uk>
2024-03-23 17:03:21 +0000

    test: :white_check_mark: add passing tests for multiple or objects

--- src/tests/objectToOrClause.test.ts
+++ src/tests/objectToOrClause.test.ts
@@ -22,4 +22,31 @@ describe('objectToOrClause tests', () => {
 
     expect(actual).toStrictEqual(expectation)
   })
+  it('converts the or objects with multiple OR clauses to a SQL OR clause', async () => {
+    const actual = objectToOrClause<TableRow>([
+      {
+        id: 1,
+        location: 'Brighton',
+      },
+    ])
+
+    const expectation = `id = '1' OR location = 'Brighton'`
+
+    expect(actual).toStrictEqual(expectation)
+  })
+  it('converts the or objects with multiple OR clauses to a SQL OR clause, multiple objects', async () => {
+    const actual = objectToOrClause<TableRow>([
+      {
+        id: 1,
+        location: 'Brighton',
+      },
+      {
+        location: 'Leeds',
+      },
+    ])
+
+    const expectation = `id = '1' OR location = 'Brighton' OR location = 'Leeds'`
+
+    expect(actual).toStrictEqual(expectation)
+  })
 })
