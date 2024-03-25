8744084500d883980e31735e098b65d8d09c496f
CRBroughton <crbroughton@posteo.uk>
2024-03-25 19:28:32 +0000

    feat: :construction: stub out functions for bun compatible sibyl

---
+++ src/bun/index.ts
@@ -0,0 +1,19 @@
+export default async function bSibyl() {
+  function createTable() {}
+  function Insert() {}
+  function Select() {}
+  function Create() {}
+  function All() {}
+  function Update() {}
+  function Delete() {}
+
+  return {
+    createTable,
+    Insert,
+    Select,
+    Create,
+    All,
+    Update,
+    Delete,
+  }
+}
