f9ca95c2596df3a868ccb8714ac39372dc6fa46a
CRBroughton <crbroughton@posteo.uk>
2024-03-27 17:48:41 +0000

    feat: :construction: setup turso client, add bun declaration file for env variables

--- turso-playground/index.ts
+++ turso-playground/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+/* eslint-disable node/prefer-global/process */
+import { createClient } from '@libsql/client'
+
+const client = createClient({
+  url: process.env.TURSO_DB_URL!,
+  authToken: process.env.TURSO_DB_AUTH_TOKEN!,
+})
