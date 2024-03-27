a7b1b1f644fa9beabaf071f4961b1a8a7924a808
CRBroughton <crbroughton@posteo.uk>
2024-03-27 17:57:49 +0000

    feat: :construction: add sibyl_bun as a starting template for turso dependencies

--- turso-playground/index.ts
+++ turso-playground/index.ts
@@ -1,7 +1,10 @@
 /* eslint-disable node/prefer-global/process */
 import { createClient } from '@libsql/client'
+import Sibyl from '@crbroughton/sibyl_bun'
 
 const client = createClient({
   url: process.env.TURSO_DB_URL!,
   authToken: process.env.TURSO_DB_AUTH_TOKEN!,
 })
+
+const {} = Sibyl(client)
